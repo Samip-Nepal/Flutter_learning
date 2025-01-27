aef66177db73807c731a283e0f360b43e319b876
Samip Nepal <samipnepal5@gmail.com>
2025-01-27 11:08:15 +0545

    Scaffold and Appbar

---
+++ my_first_app/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text('My First App'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Text('Hello World'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            // ...existing code...
+          },
+          child: Text('Click'),
+        ),
+      ),
+    ));
