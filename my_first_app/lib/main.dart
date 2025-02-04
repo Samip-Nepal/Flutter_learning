63be766e10fcba93217ecf7f58041295182b3835
Samip Nepal <samipnepal5@gmail.com>
2025-02-04 18:28:07 +0545

    assets with image and button

--- my_first_app/lib/main.dart
+++ my_first_app/lib/main.dart
@@ -1,46 +1,75 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
   runApp(MaterialApp(
     home: Myhome(),
   ));
 }
+
 class Myhome extends StatelessWidget {
+  const Myhome({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.white,
         title: Text(
-          'Hi everyone',
-          style: GoogleFonts.pacifico(
-            textStyle: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-            ),
+          "My App",
+          style: TextStyle(
+            fontSize: 20,
+            color: Color(0xFF00FF00),
+            fontWeight: FontWeight.bold,
+            fontFamily: 'Delius',
           ),
         ),
       ),
       body: Center(
-        child: Text(
-          'NAMASTE NEPAL',
-          style: GoogleFonts.openSans(
-            textStyle: TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
+          // child: Text(
+          //   "Hello World",
+          //   style: TextStyle(
+          //     fontSize: 30,
+          //     color: Color(0xFF00FF00),
+          //     fontWeight: FontWeight.bold,
+          //   ),
+          // ),
+          // child: Image.asset('assets/random.jpg'),
+          // child: TextButton(
+          //   onPressed: () {
+          //     print("Button Pressed");
+          //   },
+          //   child: Text(
+          //     "Click Me",
+          //     style: TextStyle(
+          //       fontSize: 20,
+          //       color: Color(0xFF00FF00),
+          //       fontWeight: FontWeight.bold,
+          //     ),
+          //   ),
+          // ),
+          child: ElevatedButton.icon(
+        onPressed: () {
+          print("Button Pressed");
+        },
+        icon: Icon(
+          Icons.person,
+          size: 30,
         ),
-      ),
+        label: Text(
+          "Click Me",
+        ),
+      )),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         backgroundColor: Color(0xFF00FF00),
-        child: 
-          Center(child: Text('Click me '),)
-            
+        child: Center(
+          child: Icon(
+            Icons.airport_shuttle,
+            size: 30,
+            color: Colors.white,
+          ),
+        ),
       ),
-    ) ;
+    );
   }
 }
